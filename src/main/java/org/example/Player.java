8dda0bbc59098932792e25ae801c241438a6997d
WG-7 <155075911+WG-7@users.noreply.github.com>
2024-03-15 10:49:59 +0100

    added all the Player Functions

--- src/main/java/org/example/Player.java
+++ src/main/java/org/example/Player.java
@@ -3,7 +3,22 @@ package org.example;
 public class Player {
     private String name;
     private int balance;
-    public Player(String name, int balance) {
+    public Player(String pName, int pBalance) {
+        name = pName;
+        balance = pBalance;
+    }
 
+    private String getName(){
+        return name;
+    }
+    private int getBalance(){
+        return balance;
+    }
+
+    private void setName(String pName){
+        name = pName;
+    }
+    private void setBalance(int pBalance) {
+        balance = pBalance;
     }
 }
