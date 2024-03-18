dfbda9355ad79ae61564ca2e652ea8dd433f3f75
0KommtDerWinter0 <f.sallermann@web.de>
2024-03-18 16:10:18 +0100

    -added pictures as test

--- src/main/java/org/gamblingcoltd/blackjack/HelloController.java
+++ src/main/java/org/gamblingcoltd/blackjack/HelloController.java
@@ -3,10 +3,23 @@ package org.gamblingcoltd.blackjack;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    @FXML
+    private ImageView imagedealer1;
+    @FXML
+    private ImageView imagedealer2;
+
+    @FXML
+    private void initialize() {
+        Image image = new Image(getClass().getResourceAsStream("/org/gamblingcoltd/blackjack/CardPNG/2_of_clubs.png"));
+        imagedealer1.setImage(image);
+        imagedealer2.setImage(image);
+    }
 
     @FXML
     protected void onHelloButtonClick() {
