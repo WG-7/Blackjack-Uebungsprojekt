42ab5a06bcbe0a9965897e0971c53bf0a55b3c49
0KommtDerWinter0 <f.sallermann@web.de>
2024-03-19 11:10:52 +0100

    -added style.css -added background image -styled multiple items (labels, buttons) -renamed 2 classes

---
+++ src/main/java/org/gamblingcoltd/blackjack/Main.java
@@ -0,0 +1,28 @@
+package org.gamblingcoltd.blackjack;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
+
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("");
+        stage.setTitle("Blackjack");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
