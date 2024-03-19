15789d1b20b0fd1b814024ab4a6ce44e53fb0f94
WG-7 <155075911+WG-7@users.noreply.github.com>
2024-03-19 11:43:04 +0100

    added card draw mechanic

--- src/main/java/org/gamblingcoltd/blackjack/Controller.java
+++ src/main/java/org/gamblingcoltd/blackjack/Controller.java
@@ -2,23 +2,44 @@ package org.gamblingcoltd.blackjack;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
+
+import java.util.Objects;
 
 public class Controller {
+    public HBox cardBox;
     @FXML
     private Label welcomeText;
-    @FXML
-    private ImageView imagedealer1;
-    @FXML
-    private ImageView imagedealer2;
 
-    @FXML
-    private void initialize() {
-        Image image = new Image(getClass().getResourceAsStream("/org/gamblingcoltd/blackjack/CardPNG/2_of_clubs.png"));
-        imagedealer1.setImage(image);
-        imagedealer2.setImage(image);
+    private int testCounter = 1;
+
+    public void drawCard(MouseEvent actionEvent) {
+        testCounter++;
+        Image card = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/org/gamblingcoltd/blackjack/CardPNG/"+testCounter+"_of_hearts.png")));
+        ImageView cardView = new ImageView(card);
+        cardView.setFitWidth(200);
+        cardView.setFitHeight(290.4);
+        cardView.setPreserveRatio(true);
+
+        cardBox.getChildren().add(cardView);
+        adjustCardPositions();
+    }
+
+    private void adjustCardPositions() {
+        double overlap = 160; // Ihr gewünschter Überlappungswert
+
+        double standartRightShift = (cardBox.getChildren().size()/2.0)*100;
+
+        for (int i = 0; i < cardBox.getChildren().size(); i++) {
+            Node child = cardBox.getChildren().get(i);
+            double translateX = i * overlap;
+            child.setTranslateX(-translateX+standartRightShift);
+        }
     }
 
     @FXML
--- src/main/java/org/gamblingcoltd/blackjack/Main.java
+++ src/main/java/org/gamblingcoltd/blackjack/Main.java
@@ -5,16 +5,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("view.fxml"));
         Parent root = fxmlLoader.load();
-        Scene scene = new Scene(root);
 
+        Scene scene = new Scene(root);
         stage.setFullScreen(true);
         stage.setFullScreenExitHint("");
         stage.setTitle("Blackjack");
