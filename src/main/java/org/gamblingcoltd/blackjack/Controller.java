328e6c3bb63acdbcb2a83bac065e4643a77951f3
WG-7 <155075911+WG-7@users.noreply.github.com>
2024-03-20 09:55:43 +0100

    fixed the ui to be compatable with both 768P and 1920P

--- src/main/java/org/gamblingcoltd/blackjack/Controller.java
+++ src/main/java/org/gamblingcoltd/blackjack/Controller.java
@@ -78,7 +78,7 @@ public class Controller {
         LbBalance.setText(""+currentGame.getPlayer().getBalance());
 
         // Label PlayerHand
-        handId.setText("Hand "+currentGame.getCurrentHandIndex()+1+" von "+currentGame.getPlayerHandSize());
+        handId.setText("Hand "+(currentGame.getCurrentHandIndex()+1)+" von "+currentGame.getPlayerHandSize());
         cardValuePlayer.setText("Kartenwert: "+currentGame.getCurrenPlayerHand().getHandValue());
         bet.setText("Einsatz: "+currentGame.getCurrenPlayerHand().getBet());
 
@@ -93,11 +93,20 @@ public class Controller {
         Rectangle2D screenBounds = Screen.getPrimary().getBounds();
         double screenWidth = screenBounds.getWidth();
         double screenHeight = screenBounds.getHeight();
-
+//        double screenWidth = 1080;
+//        double screenHeight = 1920;
 
         cardBoxDealer.getChildren().clear();
 
-        //Player
+        //modify according to screenHeight
+        cardBoxDealer.minHeight(screenHeight*0.2);
+        cardBoxDealer.maxHeight(screenHeight*0.2);
+        cardBoxPlayer.minHeight(screenHeight*0.2);
+        cardBoxPlayer.maxHeight(screenHeight*0.2);
+        double cardHeight = screenHeight*0.2;
+        double cardWidth = screenWidth*0.1;
+
+        //Dealer
         Hand dealerHand = currentGame.getDealerHand();
         for(int i = 0; i < dealerHand.getSize();i++)
         {
@@ -110,14 +119,12 @@ public class Controller {
                 card = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/org/gamblingcoltd/blackjack/CardPNG/"+cardUrl)));
             }
             ImageView cardView = new ImageView(card);
-            cardView.setFitWidth(screenWidth*0.1);
-            cardView.setFitHeight(screenHeight*0.2);
-            cardBoxDealer.minHeight(screenHeight*0.2);
-            cardBoxDealer.maxHeight(screenHeight*0.2);
+            cardView.setFitWidth(cardWidth);
+            cardView.setFitHeight(cardHeight);
             cardView.setPreserveRatio(true);
             cardBoxDealer.getChildren().add(cardView);
         }
-        adjustCardPositions(cardBoxDealer);
+        adjustCardPositions(cardBoxDealer,cardWidth);
 
         //Player
         cardBoxPlayer.getChildren().clear();
@@ -129,26 +136,27 @@ public class Controller {
             ImageView cardView = new ImageView(card);
 //            cardView.setFitWidth(200);
 //            cardView.setFitHeight(290.4);
-            cardView.setFitWidth(screenWidth*0.1);
-            cardView.setFitHeight(screenHeight*0.2);
-            cardBoxPlayer.minHeight(screenHeight*0.2);
-            cardBoxPlayer.maxHeight(screenHeight*0.2);
+            cardView.setFitWidth(cardWidth);
+            cardView.setFitHeight(cardHeight);
             cardView.setPreserveRatio(true);
 
             cardBoxPlayer.getChildren().add(cardView);
         }
-        adjustCardPositions(cardBoxPlayer);
+        adjustCardPositions(cardBoxPlayer, cardWidth);
     }
 
-    private void adjustCardPositions(HBox pHBox) {
+    private void adjustCardPositions(HBox pHBox,double cardWidth) {
         HBox cardBox = pHBox;
-        double overlap = 160;
+
+        double noOverlap = cardWidth*0.2;
+        double overlap = cardWidth*0.8;
+
         //(cardBoxPlayer width in px - px width of hand)/2
-        double standartRightShift = (cardBox.getWidth()-((cardBox.getChildren().size()*40)+overlap))/2;
+        double standartRightShift = (cardBox.getWidth()-((cardBox.getChildren().size()*noOverlap)+overlap))/2;
 
         for (int i = 0; i < cardBox.getChildren().size(); i++) {
             Node child = cardBox.getChildren().get(i);
-            double translateX = i * overlap;
+            double translateX = i * 80;
             child.setTranslateX(-translateX+standartRightShift);
         }
     }
